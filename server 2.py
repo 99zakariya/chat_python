5f2b493dd8dce42e84c8d0a3e7f6726460d0e2bf
zak <99zakariya@gmail.com>
2018-10-16 02:14:13 +0530

    multi user group chat

--- server 2.py
+++ server 2.py
@@ -1,17 +1,49 @@
 import socket, threading
 import time
+from _thread import start_new_thread
 
 def accept_client():
     while True:
-        #accept    
+        #accept new connection   
         cli_sock, cli_add = ser_sock.accept()
         uname = cli_sock.recv(1024)
         user.append((uname, cli_sock))
-        #wating_m.append((cli_sock,"Chat room ".encode(),((uname.decode())+' has joined the chat room').encode()))
+       
         wating_m.append((cli_sock,( "Chat room > {} has joined the chat room".format(uname.decode())).encode()))
         print('{} is now connected ip:- {}   port:- {} ' .format(uname.decode(), cli_add[0],cli_add[1]))
+        start_new_thread(rec,(uname, cli_sock)) # making dedecated thread
 
-def broadcast_usr():
+#using dedecated thread
+def rec(name,u):
+   while True:
+       try:
+           data = u.recv(1024)
+           if data:
+               wating_m.append((u,("{} > {}".format( name.decode(),data.decode())).encode()))
+       except Exception as x:
+           print(name.decode()+' left chat room')
+           wating_m.append((u,( "Chat room > {} has left the chat room".format(name.decode())).encode()))    
+           try:
+               user.remove((name,u))
+           except:
+               print('exception in removing')
+           finally:
+               break
+               pass
+    
+   print("out")               
+
+
+
+
+
+
+
+
+
+
+#prev code working with one thread
+'''def broadcast_usr():
     while True:
         for i in user:
             try:
@@ -21,15 +53,15 @@ def broadcast_usr():
             except Exception as x:
                 print(i[0].decode()+' left chat room')
                 wating_m.append((i[1],( "Chat room > {} has left the chat room".format(i[0].decode())).encode()))
-                # wating_m.append((i[1],"Chat room ".encode(),((i[0].decode())+' has left the chat room').encode()))
+                
                 try:
                     user.remove(i)
                 except:
                     print('exception in removing')
                     pass
-                continue
+                continue'''
                 
-
+#using one thread to send meaasge to all person
 def send():
     while True:
         if len(wating_m)>0:
@@ -41,7 +73,7 @@ def send():
                 except Exception as x:
                         print((i[0]).decode()+' left chat room from send')
                         wating_m.append((i[1],( "Chat room > {} has left the chat room".format(i[0].decode())).encode()))
-                        #wating_m.append((i[1],("Chat room >",(i[0].decode()),' has left the chat room').encode()))
+                       
                         try:
                             user.remove(i)
                         except:
@@ -71,8 +103,9 @@ if __name__ == "__main__":
     thread_ac = threading.Thread(target = accept_client)
     thread_ac.start()
 
-    thread_bs = threading.Thread(target = broadcast_usr)
-    thread_bs.start()
+   
     thread_bsp = threading.Thread(target = send)
     thread_bsp.start()
-    time.sleep(100000)
+    #thread_bs = threading.Thread(target = broadcast_usr) -----code of single thread
+    #thread_bs.start()  -----code of single thread
+    time.sleep(100000) #sever stays on for this amount of time
